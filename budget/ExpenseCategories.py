615f19f972784b23339e7a9acd6111c135a638cc
Michael Thomson <github.com@m-thomson.net>
2020-11-24 16:57:44 +0000

    Module 5 final

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -46,8 +46,6 @@ expenses.read_expenses('data/spending_data.csv')
     for category_exps in divided_set_comp:
         divided_expenses_sum.append(sum([x.amount for x in category_exps]))
 
-    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
-
     ax.pie(divided_expenses_sum, labels=labels, autopct='%1.1f%%')
 
     plt.show()
