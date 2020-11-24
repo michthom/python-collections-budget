44cd17594dca16028ce676fb14ed04313172fda3
Michael Thomson <github.com@m-thomson.net>
2020-11-24 16:50:40 +0000

    Module 5 pass 3

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -37,5 +37,21 @@ expenses.read_expenses('data/spending_data.csv')
     globals=globals()
     ))
 
+    fig, ax = plt.subplots()
+
+    labels = ['Necessary', 'Food', 'Unnecessary']
+
+    divided_expenses_sum = []
+
+    for category_exps in divided_set_comp:
+        divided_expenses_sum.append([x.amount for x in category_exps].sum())
+
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+
+    ax.pie(divided_expenses_sum, labels=labels, autopct='%1.1f%%')
+
+    plt.show()
+
+
 if __name__ == "__main__":
     main()
